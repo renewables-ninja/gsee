c402cc995750c2ba1aa4c2abb071b12f9d48aa76
tsaoyu <tsaoyu@tsaoyu.com>
2017-05-10 10:11:01 +0100

    Fix fill NaN issue

--- gsee/pv.py
+++ gsee/pv.py
@@ -119,7 +119,7 @@ class PVPanel(object):
                        + self.k_4 * np.log(G_)
                        + self.k_5 * (np.log(G_)) ** 2)
                + self.k_6 * (T_ ** 2))
-        eff.fillna(0)  # NaNs in case that G_ was <= 0
+        eff.fillna(0, inplace = True)  # NaNs in case that G_ was <= 0
         eff[eff < 0] = 0  # Also make sure efficiency can't be negative
         return eff
 
