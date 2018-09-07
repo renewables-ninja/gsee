67fb8f143cf4b12236ee846a297c724645a7d005
Stefan Pfenninger <stefan@pfenninger.org>
2018-09-07 11:08:08 +0200

    Improved setup.py

--- gsee/__init__.py
+++ gsee/__init__.py
@@ -1,6 +1,6 @@
-__title__ = 'gsee -- global solar energy estimator'
+__title__ = 'GSEE -- Global Solar Energy Estimator'
 __author__ = 'Stefan Pfenninger'
 
 from gsee._version import __version__
 
-from gsee import brl_model, pv
+from gsee import brl_model, pv, csp_thermal, csp_electric, powerblock_cooling
--- setup.py
+++ setup.py
@@ -1,38 +1,68 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
-from Cython.Build import cythonize #added
-import numpy
+from setuptools import setup, find_packages, Extension
 
 # Sets the __version__ variable
-exec(open('gsee/_version.py').read())
+with open('gsee/_version.py', 'r') as f:
+    exec(f.read())
 
-with open("README.md", "r") as f:
+with open('README.md', 'r') as f:
     long_description = f.read()
+
+# If Cython is available, cythonize the .pyx module to a C source file.
+# Else, use the pre-compiled C module.
+try:
+    from Cython.Build import cythonize
+    # If Cython is available, numpy must be available too.
+    import numpy as np
+except ImportError:
+    print('Attempting to compile pre-built Cython module')
+    from setuptools.command.build_ext import build_ext
+    ext_modules = [Extension(
+        "gsee/climatedata_interface/*",
+        ["gsee/climatedata_interface/*.c"]
+    )]
+else:
+    print('Attempting to build Cython module from Cython source')
+    ext_modules = cythonize([
+        Extension(
+            "gsee/climatedata_interface/*",
+            ["gsee/climatedata_interface/*.pyx"],
+            include_dirs=[np.get_include()]
+        )
+    ])
 
 setup(
     name='gsee',
     version=__version__,
     author='Stefan Pfenninger',
     author_email='stefan@pfenninger.org',
-    description='GSEE: Global Solar Energy Estimator, including climate data interface',
+    description='GSEE: Global Solar Energy Estimator',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/renewables-ninja/gsee',
     packages=find_packages(),
+    include_package_data=True,
+    ext_modules=ext_modules,
+    zip_safe=False,
     install_requires=[
-        "pyephem >= 3.7.6.0",
-        "numpy >= 1.10.1",
-        "pandas >= 0.18.0",
-        "joblib >= 0.11",
-        "scipy >= 0.19.1",
-        "xarray >= 0.10.7",
-        "Cython >= 0.27.3"
+        "numpy >= 1.15",
+        "pandas >= 0.23",
+        "pyephem >= 3.7.6",
+        "scipy >= 1.1",
+        "xarray >= 0.10.8",
     ],
+    setup_requires=[
+        'Cython >= 0.27.3',
+        "numpy >= 1.15",
+    ],
+    extras_require={
+        'multicore': ["joblib >= 0.12"],
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
     ],
-    ext_modules = cythonize("gsee/climdata_interface/cyth_func.pyx"), #added
-    include_package_data=True,
-    include_dirs=[numpy.get_include()]
 )
