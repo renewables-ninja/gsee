6574a7707d9c1374ebb32aab2c07f9d185f25a99
Stefan Pfenninger <stefan@pfenninger.org>
2024-04-29 09:06:37 +0200

    Clean up and reorganise
    * Move source to src, tests to tests
    * Remove climate data interface
    * Move to pyproject.toml for all metadata

--- gsee/__init__.py
+++
@@ -1,6 +0,0 @@
-__title__ = "GSEE -- Global Solar Energy Estimator"
-__author__ = "Stefan Pfenninger"
-
-from gsee._version import __version__
-
-from gsee import brl_model, pv
---
+++ src/gsee/__init__.py
@@ -0,0 +1,3 @@
+from gsee._version import __version__
+
+from gsee import brl_model, pv
